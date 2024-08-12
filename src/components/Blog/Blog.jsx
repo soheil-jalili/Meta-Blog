aa0405c31df9eee422ed67d0e4870fb1b5f95aa1
soheil14 <jalilisoheil14@gmail.com>
2024-08-12 12:46:55 -0700

    add advertisement

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,17 +2,14 @@ import styles from "./Blog.module.css";
 import Nav from "../widgets/Nav/Nav";
 import BlogBanner from "../Blog_Banner/Blog_Banner";
 import Advertisement from "../Advertisement/Advertisement";
+import Header from "./../Header/Header";
+import Main from "../Main/Main";
 
 function Blog() {
   return (
     <>
-      <header>
-        <Nav />
-      </header>
-      <main>
-        <BlogBanner />
-        <Advertisement />
-      </main>
+      <Header />
+      <Main />
     </>
   );
 }
