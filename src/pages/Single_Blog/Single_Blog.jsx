51db6da8a1c89d397c3ee016a99d026b6699e0d2
soheil14 <jalilisoheil14@gmail.com>
2024-08-13 07:40:24 -0700

    add badge to single blog page

--- src/pages/Single_Blog/Single_Blog.jsx
+++ src/pages/Single_Blog/Single_Blog.jsx
@@ -1,9 +1,16 @@
+import Badge from "../../widgets/Badge/Badge";
 import Nav from "./../../components/Nav/Nav";
 function SingleBlog() {
   return (
     <>
       <div className="container">
         <Nav />
+        <Badge
+          backgroundColor={"#4B6BFB"}
+          link={"/"}
+          textColor={"#fff"}
+          title={"Technology"}
+        />
       </div>
     </>
   );
