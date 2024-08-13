314ba7949770428425302ed40f07dc499fd9567d
soheil14 <jalilisoheil14@gmail.com>
2024-08-13 09:34:55 -0700

    add style to paragraph body single

--- src/pages/Single_Blog/Body_Single_Blog/Body_Single_Blog.jsx
+++ src/pages/Single_Blog/Body_Single_Blog/Body_Single_Blog.jsx
@@ -5,6 +5,7 @@ import tracey from "../../../assets/img/profile__photo/tracey.png";
 
 import banner from "../../../assets/img/banner.png";
 import Footer from "../../../components/Footer/Footer";
+import Advertisement from "../../../components/Advertisement/Advertisement";
 
 function BodySingleBlog() {
   return (
@@ -27,11 +28,15 @@ function BodySingleBlog() {
         However, traveling can also be stressful and overwhelming, especially if
         you don't plan and prepare adequately. In this blog article, we'll
         explore tips and tricks for a memorable journey and how to make the most
-        of your travels. One of the most rewarding aspects of traveling is
-        immersing yourself in the local culture and customs. This includes
-        trying local cuisine, attending cultural events and festivals, and
-        interacting with locals. Learning a few phrases in the local language
-        can also go a long way in making connections and showing respect.
+        of your travels.
+      </p>
+
+      <p className={`${styles.blog__single__paragraph} ${styles.mt__2}`}>
+        One of the most rewarding aspects of traveling is immersing yourself in
+        the local culture and customs. This includes trying local cuisine,
+        attending cultural events and festivals, and interacting with locals.
+        Learning a few phrases in the local language can also go a long way in
+        making connections and showing respect.
       </p>
 
       <h3 className={styles.blog__single__paragraph__main__title}>
@@ -43,12 +48,15 @@ function BodySingleBlog() {
         destination. This includes understanding the local culture, customs, and
         laws, as well as identifying top attractions, restaurants, and
         accommodations. Doing so will help you navigate your destination with
-        confidence and avoid any cultural faux pas. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus
-        orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit
-        amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae.
-        Viverra adipiscing at in tellus.
+        confidence and avoid any cultural faux pas.
+      </p>
+
+      <p className={`${styles.blog__single__paragraph} ${styles.mt__2}`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida
+        rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae
+        aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis
+        eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.
       </p>
       <h3 className={styles.blog__single__paragraph__main__title}>
         Plan Your Itinerary
@@ -60,10 +68,79 @@ function BodySingleBlog() {
         time and budget. Identify the must-see sights and experiences and
         prioritize them according to your interests and preferences. This will
         help you avoid overscheduling and ensure that you have time to relax and
-        enjoy your journey. Vitae sapien pellentesque habitant morbi tristique.
-        Luctus venenatis lectus magna fringilla. Nec ullamcorper sit amet risus
-        nullam eget felis. Tincidunt arcu non sodales neque sodales ut etiam sit
-        amet.
+        enjoy your journey.
+      </p>
+
+      <p className={`${styles.blog__single__paragraph} ${styles.mt__2}`}>
+        Vitae sapien pellentesque habitant morbi tristique. Luctus venenatis
+        lectus magna fringilla. Nec ullamcorper sit amet risus nullam eget
+        felis. Tincidunt arcu non sodales neque sodales ut etiam sit amet.
+      </p>
+
+      <img src={banner} alt="" className={styles.single__blog__banner1} />
+
+      <Advertisement
+        marginBottom={"2rem"}
+        marginRight={"auto"}
+        marginLeft={"auto"}
+      />
+
+      <h3 className={styles.blog__single__paragraph__main__title}>
+        Pack Lightly and Smartly
+      </h3>
+
+      <p className={styles.blog__single__paragraph}>
+        Packing can be a daunting task, but with some careful planning and smart
+        choices, you can pack light and efficiently. Start by making a packing
+        list and sticking to it, focusing on versatile and comfortable clothing
+        that can be mixed and matched. Invest in quality luggage and packing
+        organizers to maximize space and minimize wrinkles.
+      </p>
+
+      <h3 className={styles.blog__single__paragraph__main__title}>
+        Stay Safe and Healthy
+      </h3>
+
+      <p className={styles.blog__single__paragraph}>
+        Traveling can expose you to new environments and potential health risks,
+        so it's crucial to take precautions to stay safe and healthy. This
+        includes researching any required vaccinations or medications, staying
+        hydrated, washing your hands frequently, and using sunscreen and insect
+        repellent. It's also essential to keep your valuables safe and secure
+        and to be aware of your surroundings at all times.
+      </p>
+      <h3 className={styles.blog__single__paragraph__main__title}>
+        Immerse Yourself in the Local Culture{" "}
+      </h3>
+
+      <p className={styles.blog__single__paragraph}>
+        One of the most rewarding aspects of traveling is immersing yourself in
+        the local culture and customs. This includes trying local cuisine,
+        attending cultural events and festivals, and interacting with locals.
+        Learning a few phrases in the local language can also go a long way in
+        making connections and showing respect.
+      </p>
+      <h3 className={styles.blog__single__paragraph__main__title}>
+        Capture Memories
+      </h3>
+
+      <p className={styles.blog__single__paragraph}>
+        Finally, don't forget to capture memories of your journey. Whether it's
+        through photographs, journaling, or souvenirs, preserving the moments
+        and experiences of your travels can bring joy and nostalgia for years to
+        come. However, it's also essential to be present in the moment and not
+        let technology distract you from the beauty of your surroundings.
+      </p>
+
+      <h3 className={styles.blog__single__paragraph__main__title}>
+        Conclusion:
+      </h3>
+
+      <p className={`${styles.blog__single__paragraph} ${styles.mb__5}`}>
+        Traveling is an art form that requires a blend of planning, preparation,
+        and spontaneity. By following these tips and tricks, you can make the
+        most of your journey and create memories that last a lifetime. So pack
+        your bags, embrace the adventure, and enjoy the ride.
       </p>
     </>
   );
--- src/pages/Single_Blog/Single_Blog.jsx
+++ src/pages/Single_Blog/Single_Blog.jsx
@@ -1,5 +1,5 @@
 import Footer from "../../components/Footer/Footer";
-
+import styles from "./Single_Blog.module.css";
 import HeaderSingleBlog from "./Header_Single_Blog/Header_Single_Blog";
 import MainSingleBlog from "./Main_Single_Blog/Main_Single_Blog";
 
@@ -8,6 +8,9 @@ function SingleBlog() {
     <>
       <div className="container">
         <HeaderSingleBlog />
+      </div>
+
+      <div className={styles.single__blog__container}>
         <MainSingleBlog />
       </div>
       <Footer />
