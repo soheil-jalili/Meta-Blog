d5d8c156f5c419a803c33b8b28c25e016e106a0c
soheil14 <jalilisoheil14@gmail.com>
2024-08-13 07:38:43 -0700

    add nav to single blog page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SingleBlog from "./pages/Single_Blog/Single_Blog.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<SingleBlog />} />
         <Route path="/:blog_id" element={<App />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Single_Blog/Single_Blog.jsx
@@ -0,0 +1,12 @@
+import Nav from "./../../components/Nav/Nav";
+function SingleBlog() {
+  return (
+    <>
+      <div className="container">
+        <Nav />
+      </div>
+    </>
+  );
+}
+
+export default SingleBlog;
